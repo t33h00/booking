edd02ef0bcf56be1c2ed3fc1e5aae5155a66baa4
t33h00 <t33h00@gmail.com>
2023-10-17 20:44:25 -0400

    query

--- src/main/java/com/lotus/booking/Repository/TransactionRepository.java
+++ src/main/java/com/lotus/booking/Repository/TransactionRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    public static final String calendarView = "SELECT SUM(t.amount) as 'amount', t.date  FROM Transactions t WHERE t.user_id = ?1 GROUP BY t.date";
+    public static final String calendarView = "SELECT SUM(t.amount) as amount, t.date  FROM Transactions t WHERE t.user_id = ?1 GROUP BY t.date";
     @Query(value = "SELECT * FROM Transactions t WHERE t.user_id =?1 and t.date =?2", nativeQuery = true)
     List<Transaction> findByUserIdAndDate(Long user_id, String date);
     @Query(value = "SELECT * FROM Transactions t WHERE t.user_id =?1", nativeQuery = true)

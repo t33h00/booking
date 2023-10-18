3b7c2080fc2630298d924f5bc0bed5f03a52d704
t33h00 <t33h00@gmail.com>
2023-10-18 08:52:41 -0400

    changed

--- src/main/java/com/lotus/booking/Config/SecurityConfig.java
+++ src/main/java/com/lotus/booking/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     private JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf->csrf.disable());g
+        http.csrf(csrf->csrf.disable());
         http.formLogin(fl->
                 fl.usernameParameter("username").passwordParameter("password"));
         http.authorizeHttpRequests(auth->{

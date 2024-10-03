bfd511077c9c382a5d7fa51533e136d8f31713d6
t33h00 <t33h00@gmail.com>
2024-10-03 12:45:04 -0400

    03/22

--- src/main/java/com/lotus/booking/Service/CheckInServiceImpl.java
+++ src/main/java/com/lotus/booking/Service/CheckInServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;

b908f012215c608df2403624ddf3a18b4c28258e
Tee Ho <teeho@Tees-MacBook-Pro.local>
2024-12-05 09:48:49 -0500

    “12/05”

--- src/main/java/com/lotus/booking/Config/SecurityConfig.java
+++ src/main/java/com/lotus/booking/Config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(fl->
                 fl.usernameParameter("username").passwordParameter("password"));
-        http.csrf(csrf->csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.cors(c->c.configurationSource(corsConfigurationSource()));
         http.authorizeHttpRequests(auth->{
             auth.requestMatchers("/auth/**").permitAll();
